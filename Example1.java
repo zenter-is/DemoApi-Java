13ae430e7245283417e2aa0e6e3f776956d60602
Ingimar Svanberg Jóhannesson <ingimar.johannesson@gmail.com>
2018-10-31 18:16:15 +0000

    added missing imports to example1

--- Example1.java
+++ Example1.java
@@ -1,3 +1,10 @@
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 
 public class Example1
 {
